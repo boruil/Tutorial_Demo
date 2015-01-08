9e178021f7feef0762475558e79e842b4dbd7082
Andy <boruil@cs.washington.edu>
2015-01-07 23:14:27 -0800

    add the new activity for send button

--- app/src/main/java/com/boruili/tutorial_demo/DisplayMessageActivity.java
+++ app/src/main/java/com/boruili/tutorial_demo/DisplayMessageActivity.java
@@ -1,5 +1,6 @@
 package com.boruili.tutorial_demo;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
@@ -10,7 +11,8 @@ public class DisplayMessageActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_display_message);
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
     }
 
     @Override
