9721c320b2d8afb07a0e0ce25c774fb715737ee5
Andy <boruil@cs.washington.edu>
2015-01-07 22:00:58 -0800

    add functions for send button

---
+++ app/src/main/java/com/boruili/tutorial_demo/DisplayMessageActivity.java
@@ -0,0 +1,7 @@
+package com.boruili.tutorial_demo;
+
+/**
+ * Created by Andy on 1/7/15.
+ */
+public class DisplayMessageActivity {
+}
--- app/src/main/java/com/boruili/tutorial_demo/MainActivity.java
+++ app/src/main/java/com/boruili/tutorial_demo/MainActivity.java
@@ -1,9 +1,12 @@
 package com.boruili.tutorial_demo;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -35,5 +38,17 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * called when the user clicks the Send button
+     * @param view the View that was clicked
+     */
+
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+//        intent.putExtra(EXTRA_MESSAGE, message);
     }
 }
